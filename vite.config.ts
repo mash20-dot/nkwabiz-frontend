55132f2b3c65ed98f0bfc8bd4f24143365eec1de
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2025-10-11 13:11:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
+    host: "0.0.0.0",
+    allowedHosts: [
+      "nkwabiz-frontend.onrender.com"
+    ]
+  }
 })
