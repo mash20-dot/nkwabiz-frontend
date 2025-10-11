a34032e92f9dcd38fe248a22a576470f7b1b7843
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2025-10-11 14:26:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
     host: "0.0.0.0",
     allowedHosts: [
-      "nkwabiz-frontend.onrender.com"
+      "nkwabiz-frontend.onrender.com",
+      "nkwabiz.com"
     ]
   }
 })
