446b3a22b424b0f2a03541036619c7722ae01b01
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2025-10-16 01:00:19 +0100

    Updating the codes

--- src/components/LandingPage/Footer.tsx
+++ src/components/LandingPage/Footer.tsx
@@ -145,7 +145,7 @@ const Footer = () => {
         </div>
         <div className="mt-12 border-t border-gray-700 pt-8">
           <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2023 NkwaBiz. All rights reserved.
+            &copy; 2025 NkwaBiz. All rights reserved.
           </p>
         </div>
       </div>
