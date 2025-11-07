e05f1ef7d9d67d513f55ea29a5a32a56d8ed9c67
Shakirat Akanji <akanjishakirato@gmail.com>
2025-11-07 16:23:53 +0100

    Update the sidebar to inclue SMS and Customers page Install untitled ui package for tables Design the layout for the SMS page

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
     host: "0.0.0.0",
     allowedHosts: ["nkwabiz-frontend-1.onrender.com", "nkwabiz.com"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
