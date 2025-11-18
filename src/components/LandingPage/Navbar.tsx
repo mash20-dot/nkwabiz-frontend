aeaf28ed29480df8044719a5792b5f3b9a27dd66
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-11-18 20:26:35 +0000

    reduced logo size

--- src/components/LandingPage/Navbar.tsx
+++ src/components/LandingPage/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0 flex items-center">
               <img
-                className="h-30 w-auto object-contain"
+                className="h-25 w-auto object-contain"
                 src="https://i.imgur.com/bFjIgob.png"
                 alt="Nkwabiz-Logo"
               />
