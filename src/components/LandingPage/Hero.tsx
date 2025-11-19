026a6f9e5a49cf4894e3aef967314f7153d11555
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-11-19 16:26:25 +0000

    changed H1 tag for SEO

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -13,15 +13,13 @@ const Hero = () => {
           <div className="pt-10 mx-auto max-w-7xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-20 lg:px-8 xl:pt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Simplify Your</span>{' '}
+                <span className="block xl:inline">Ghana Business Management System</span>{' '}
                 <span className="block text-blue-600 xl:inline">
-                  Business Management
+                  Sales & Inventory Tracking Software
                 </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                NkwaBiz helps small businesses manage inventory, track sales,
-                and gain valuable insights through analytics - all in one
-                intuitive platform designed for African entrepreneurs.
+                Track expenses, manage products, generate reports, and make data-driven decisions - all from one simple platform.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
