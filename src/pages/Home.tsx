4331443ffaaeda0666e92bd6df47447acca51ca1
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-11-21 15:26:34 +0000

    updated home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Navbar from '../components/LandingPage/Navbar';
 import Hero from '../components/LandingPage/Hero';
 import Features from '../components/LandingPage/Features';
@@ -14,31 +14,54 @@ import Feedback from '../components/LandingPage/Feedback';
 import FloatingFeedbackButton from '../components/LandingPage/FloatingFeedbackButton';
 
 const Home = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Helmet>
-        <title>Nkwabiz – Ghana business management system</title>
-        <meta
-          name="description"
-          content="Nkwabiz is a sales, inventory, bulk sms system helping business grow faster and easier."
-        />
-        <link rel="canonical" href="https://www.nkwabiz.com/" />
-      </Helmet>
+  const pageTitle = "Nkwabiz – Ghana Best Business Management Software";
+  const pageDescription = "Nkwabiz is a sales, inventory, bulk sms platform helping businesses grow faster and easier.";
+  const pageURL = "https://www.nkwabiz.com/";
+  const pageImage = "https://www.nkwabiz.com/og-image.png"; // Replace with your OG image
 
-      <Navbar />
-      <main className="flex-grow">
-        <Hero />
-        <HowItWorks />
-        <Features />
-        <Youtubesection />
-        <Benefits />
-        <Testimonials />
-        <FAQ />
-        <Feedback />
-        {/* <CTA /> */}
-      </main>
-      <Footer />
-    </div>
+  return (
+    <HelmetProvider>
+      <div className="flex flex-col min-h-screen">
+        <Helmet>
+          {/* Basic SEO */}
+          <title>{pageTitle}</title>
+          <meta name="description" content={pageDescription} />
+          <link rel="canonical" href={pageURL} />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:title" content={pageTitle} />
+          <meta property="og:description" content={pageDescription} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={pageURL} />
+          <meta property="og:image" content={pageImage} />
+
+          {/* Twitter */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={pageTitle} />
+          <meta name="twitter:description" content={pageDescription} />
+          <meta name="twitter:image" content={pageImage} />
+
+          {/* Viewport */}
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Helmet>
+
+        {/* Page Sections */}
+        <Navbar />
+        <main className="flex-grow">
+          <Hero />
+          <HowItWorks />
+          <Features />
+          <Youtubesection />
+          <Benefits />
+          <Testimonials />
+          <FAQ />
+          <Feedback />
+          {/* <CTA /> */}
+        </main>
+        <Footer />
+        <FloatingFeedbackButton />
+      </div>
+    </HelmetProvider>
   );
 };
 
