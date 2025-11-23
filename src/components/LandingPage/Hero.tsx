abbe3cf0f52e739eb59854a95621a28d21008e37
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-11-23 18:56:23 +0000

    rewrote hero

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
           <div className="pt-10 mx-auto max-w-7xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-20 lg:px-8 xl:pt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Ghana Business Management System</span>{' '}
+                <span className="block xl:inline">Best Business Management System</span>{' '}
                 <span className="block text-blue-600 xl:inline">
                   Sales & Inventory Tracking Software
                 </span>
