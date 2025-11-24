aed36741ce4a7acb38be878100f989022f5ea1e1
Shakirat Akanji <akanjishakirato@gmail.com>
2025-11-24 16:49:55 +0100

    Add Untitled Design Table components Design SMS Dashboard Layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
@@ -99,6 +100,11 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     preview: {
       port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
       host: "0.0.0.0",
